949c0423b929aba6558a68940abf79cdc58ae815
Baptiste Bidaux <baptistelise@orange.fr>
2020-05-25 18:08:51 +0200

    Ajout d'un message si pas encore de meilleur score + responsive iPhone SE

--- highscores.js
+++ highscores.js
@@ -1,5 +1,18 @@
 const highScoresList = document.getElementById('high-scores-list');
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+const title = document.getElementById('final-score');
+
+if (highScores.length == 0) {
+    const p = document.createElement("p");
+    p.classList.add("no-score");
+    p.textContent = "Pas encore de score enregistré !";
+    title.parentNode.insertBefore(p, title.nextSibling);
+} else {
+    const noScore = document.getElementsByClassName('no-score');
+    if (noScore.length != 0) {
+        noScore.parentNode.removeChild(noScore);
+    }
+}
 
 highScoresList.innerHTML =
     highScores
